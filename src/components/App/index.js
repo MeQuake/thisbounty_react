5124aaeeb74aa47aafb587f9c2793429e28b15b4
Adam Mankowski <maniekmanfred69@gmail.com>
2017-04-13 20:30:26 +0200

    Add skeleton, styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,7 +5,11 @@ require('./index.css');
 export default class AppComponent extends React.Component {
   render() {
     return (
-      <nav className="AppComponent"><ul><li>test</li></ul></nav>
+      <section id="points">
+	  /* top banner */
+	  
+	  /* points-content */
+      </section>
     );
   }
 }
