cf399aa4ba09fb243fc1e491357f6011dc17ff00
Adam Mankowski <maniekmanfred69@gmail.com>
2017-04-13 21:40:09 +0200

    Add arrow box

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TopBanner from '../TopBanner/index';
+import ArrowBox from '../ArrowBox/index';
 
 require('./index.css');
 
@@ -8,6 +9,7 @@ export default class AppComponent extends React.Component {
     return (
       <section id="points">
         <TopBanner />
+        <ArrowBox />
       </section>
     );
   }
---
+++ src/components/ArrowBox/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+require('./index.css');
+
+export default class TopBanner extends React.Component {
+  render() {
+    return (
+      <div className="arrow_box"></div>
+    );
+  }
+}
