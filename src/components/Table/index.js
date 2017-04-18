2ca0360e3c316062a39e7c7838d0779440bfab8e
Steve <urgent.programmer@gmail.com>
2017-04-18 00:17:22 -0400

    Table -- Bounty Columns, fa
    Updated table data to use mock bounty data, not react-bootstrap-table examples.
    
    Tried adding fontawesome, not showing.

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,17 +1,23 @@
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 require('./index.css');
+require('./fa.min.js');
 
-const products = [
+const bounty = [
   {
-    id: 1,
-    name: 'Product1',
-    price: 120,
-  }, {
-    id: 2,
-    name: 'Product2',
-    price: 80,
+    date: '17-Apr-2017 20:41 EST',
+    name: 'thisbounty.com -- Login',
+    descr: 'Try using this for FB login: https://www.npmjs.com/package/cognito-helper',
+    price: '$25.00',
+    claim: '0',
+    review: '0',
+    resources: '<i class="fa fa-github" aria-hidden="true"></i>',
+    action: '',
   },
 ];
 
-export { BootstrapTable, TableHeaderColumn, products };
+function actionFormatter(cell) {
+  return `<i class='fa fa-flag' aria-hidden='true' alt='Claim' data-bountyId='${cell}'></i>`;
+}
+
+export { BootstrapTable, TableHeaderColumn, bounty, actionFormatter };
--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { render } from 'react-dom';
 import ssr from './utilities/ssr/index';
 import App from './components/App/index';
-import { products, BootstrapTable, TableHeaderColumn } from './components/Table/index';
+import { bounty, actionFormatter, BootstrapTable, TableHeaderColumn } from './components/Table/index';
 
 if (typeof document === 'object') {
   render(<App />, document.getElementById('root'));
   render(
-    <BootstrapTable data={products} striped hover>
-        <TableHeaderColumn isKey dataField="id">Product ID</TableHeaderColumn>
-        <TableHeaderColumn dataField="name">Product Name</TableHeaderColumn>
-        <TableHeaderColumn dataField="price">Product Price</TableHeaderColumn>
+    <BootstrapTable data={bounty} striped hover>
+        <TableHeaderColumn isKey dataField="date">Date</TableHeaderColumn>
+        <TableHeaderColumn dataField="name">Name</TableHeaderColumn>
+        <TableHeaderColumn dataField="descr">Descr</TableHeaderColumn>
+        <TableHeaderColumn dataField="price">Price</TableHeaderColumn>
+        <TableHeaderColumn dataField="claim">Claims</TableHeaderColumn>
+        <TableHeaderColumn dataField="review">Reviewing</TableHeaderColumn>
+        <TableHeaderColumn dataField="resource">Resources</TableHeaderColumn>
+        <TableHeaderColumn dataField="action" dataFormat={ actionFormatter }>Actions</TableHeaderColumn>
     </BootstrapTable>,
     document.getElementById('table'),
   );
