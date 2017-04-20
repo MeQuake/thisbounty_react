137484f252e1a1c23ddf18ffab7ae987591c65c7
Adam Mankowski <maniekmanfred69@gmail.com>
2017-04-20 21:09:37 +0200

    Bring back support for eslint os specified linebreak
    neutrino-middleware-eslint wasn't compatible with Neutrino v4, so we had
    to use something different. Now, after upgrade to v5 we received support
    for neutrino-middleware-eslint. Thic commit brings back support for
    eslint os specified linebreak.

--- src/presets/lint-preset.js
+++ src/presets/lint-preset.js
@@ -1,30 +1,50 @@
-const lint = require('neutrino-lint-base');
-const merge = require('deepmerge');
+const eslint = require('neutrino-middleware-eslint');
+const neutrino = require('neutrino');
 
-module.exports = neutrino => {
-  // Implement custom linting
-  lint(neutrino);
-  neutrino.config.module
-    .rule('lint')
-    .loader('eslint', props => merge(props, {
-      options: {
-        globals: ['describe', 'expect', 'jest', 'test', 'document', 'window', 'fetch'],
-        rules: {
-          // Don't require () for single argument arrow functions
-          'arrow-parens': 'off',
-          // Don't require trailing commas
-          'comma-dangle': 'off',
-          // Don't require file extensions on imports
-          'import/extensions': 'off',
-          // Don't mark as unresolved without extensions
-          'import/no-unresolved': 'off',
-          // Don't let ESLint tell us how to use whitespace for imports
-          'padded-blocks': 'off',
-          // Hold off on propTypes for now
-          'react/prop-types': 'off',
-		  // Change linebreak style depending which OS is being used
-		  "linebreak-style": ["error", process.env.OS === 'windows' ? "windows" : "unix"]
+module.exports = (neutrino) => {
+  // Usage shows default values
+  neutrino.use(eslint, {
+    test: /\.(js|jsx)$/,
+    include: [], /* Should specify either include or exclude */
+    exclude: [], /* Should specify either include or exclude */
+    eslint: {
+      failOnError: process.env.NODE_ENV !== 'development',
+      emitWarning: process.env.NODE_ENV !== 'development',
+      emitError: process.env.NODE_ENV !== 'development',
+      cwd: neutrino.options.root,
+      useEslintrc: false,
+      root: true,
+      plugins: ['babel'],
+      baseConfig: {},
+      envs: ['es6'],
+      parser: 'babel-eslint',
+      parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module',
+        ecmaFeatures: {
+          objectLiteralDuplicateProperties: false,
+          generators: true,
+          impliedStrict: true
         }
+      },
+      settings: {},
+      globals: ['process'],
+      rules: {
+        // Don't require () for single argument arrow functions
+        'arrow-parens': 'off',
+        // Don't require trailing commas
+        'comma-dangle': 'off',
+        // Don't require file extensions on imports
+        'import/extensions': 'off',
+        // Don't mark as unresolved without extensions
+        'import/no-unresolved': 'off',
+        // Don't let ESLint tell us how to use whitespace for imports
+        'padded-blocks': 'off',
+        // Hold off on propTypes for now
+        'react/prop-types': 'off',
+        // Change linebreak style depending which OS is being used
+        "linebreak-style": ["error", process.env.OS === 'windows' ? "windows" : "unix"]
       }
-    }))
+    }
+  });
 };
